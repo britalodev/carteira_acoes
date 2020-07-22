7c9d02d64308a02082bc5e39f8307dc322518545
Italo P. Barbosa <italo@ideaalize.online>
2020-07-22 11:20:51 -0300

    First commit

---
+++ src/main/java/br/italo/carteirainvestimentos/CarteiraInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package br.italo.carteirainvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/italo/carteirainvestimentos/CarteiraInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.italo.carteirainvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
